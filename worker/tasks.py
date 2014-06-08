035aa7eb0bb3415fb54a375f8f6c2b2110196c6a
Spencer Herzberg <spencer.herzberg@gmail.com>
2014-06-08 22:36:01 +0000

    replace logstash redis input with direct to logstash connection

--- worker/tasks.py
+++ worker/tasks.py
@@ -1,8 +1,9 @@
 import os
+import uuid
 from celery import Celery
 import json
-import socket
-import pika
+import elasticsearch
+from datetime import datetime
 
 from processors import timestamp, hashtags, urls, domains
 
@@ -33,10 +34,13 @@ def process_pipeline(data):
 
 
 def send_to_logstash(data):
-    server_address = os.environ['RABBIT_1_PORT_5672_TCP_ADDR']
+    host = os.environ['LOGSTASH_1_PORT_9200_TCP_ADDR']
 
-    connection = pika.BlockingConnection(pika.ConnectionParameters(server_address))
-    channel = connection.channel()
-    channel.queue_declare(queue='logstash')
-    channel.basic_publish(exchange='', routing_key='logstash', body=json.dumps(data))
-    connection.close()
+    es = elasticsearch.Elasticsearch([host])
+    date = datetime.now().strftime("%Y.%m.%d")
+    es.index(
+        index='logstash-{date}'.format(date=date),
+        doc_type='logstash',
+        body=data,
+        id=uuid.uuid4()
+    )
